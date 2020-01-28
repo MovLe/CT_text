7c3b26a8012156a3df2cc034415e49a80b81856b
Movle <richard.xiu.520@gmail.com>
2020-01-28 19:20:51 +0800

    生成数据？

--- ct_producer/src/main/java/producer/ProductLog.java
+++ ct_producer/src/main/java/producer/ProductLog.java
@@ -185,7 +185,7 @@ public class ProductLog {
     }
 
     public static void main(String[] args) {
-        args =new String[]{"/Users/macbook/TestInfo/calllog.csv"};
+        //args =new String[]{"/Users/macbook/TestInfo/calllog.csv"};
 
         if(args==null||args.length<=0){
             System.out.println("没写路径");
